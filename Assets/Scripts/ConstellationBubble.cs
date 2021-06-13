22de2388d4f332cdd4b6661fbbde9c270a51b957
Epord <pedro.balaguer@hotmail.com>
2021-06-12 23:35:53 -0700

    Add constellation bubble

---
+++ Assets/Scripts/ConstellationBubble.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstellationBubble : MonoBehaviour
+{
+    public Sprite[] constellations;
+    public SpriteRenderer currentConstelationRenderer;
+    public GameObject prev;
+    public GameObject next;
+
+    private int currentConstellationIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.currentConstelationRenderer.sprite = constellations[0];
+        this.currentConstellationIndex = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                GameObject collider = hit.collider.gameObject;
+                if (collider.Equals(prev))
+                {
+                    this.PrevConstellation();
+                }
+                else if (collider.Equals(next))
+                {
+                    this.NextConstellation();
+                }
+            }
+        }
+    }
+
+    void PrevConstellation()
+    {
+        this.currentConstellationIndex = (this.currentConstellationIndex - 1) % constellations.Length;
+        this.currentConstelationRenderer.sprite = constellations[this.currentConstellationIndex];
+    }
+
+    void NextConstellation()
+    {
+        this.currentConstellationIndex = (this.currentConstellationIndex + 1) % constellations.Length;
+        this.currentConstelationRenderer.sprite = constellations[this.currentConstellationIndex];
+    }
+}
